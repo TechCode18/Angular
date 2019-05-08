0ba69058a16389df3115da3db2c13be49add7956
Biswanath Pradhan <bispradh@in.ibm.com>
2019-05-08 23:24:19 +0530

    two way databinding commiting

--- AngularDemo/src/app/app.module.ts
+++ AngularDemo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +23,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     RouterModule.forRoot(
       routes,
